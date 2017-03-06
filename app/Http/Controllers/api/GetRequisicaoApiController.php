e7389c92023acde8a13f711d62da988792d32d06
Robson Coutinho <r.ramoscoutinho@gmail.com>
2017-03-06 17:25:25 -0300

    Criação de controllers para requisições GET e POST da API

---
+++ app/Http/Controllers/api/GetRequisicaoApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GetRequisicaoApiController extends Controller
+{
+
+    public function getProdutosMarcas()
+    {
+
+    }
+
+    public function getPromocoes()
+    {
+
+    }
+
+
+    public function getCategorias()
+    {
+
+    }
+
+
+
+
+
+}
---
+++ app/Http/Controllers/api/PostRequisicaoApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostRequisicaoApiController extends Controller
+{
+    public function postPedido()
+    {
+
+    }
+
+    public function postCancelarPedidos()
+    {
+
+    }
+}
