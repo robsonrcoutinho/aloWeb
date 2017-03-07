8a656c48455ad90c5e128613dd72e08d66a9afa6
Robson Coutinho <r.ramoscoutinho@gmail.com>
2017-03-07 15:25:10 -0300

    correção: getCategorias

--- app/Http/Controllers/api/GetRequisicaoApiController.php
+++ app/Http/Controllers/api/GetRequisicaoApiController.php
@@ -22,14 +22,32 @@ class GetRequisicaoApiController extends Controller
 
     public function getPromocoes()
     {
+
+     /*   try {
+            $token = JWTAuth::parseToken();
+            if (! $token->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (TokenInvalidException $e) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (JWTException $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        //$user = JWTAuth::parseToken()->authenticate();
+        //$user = User::find(1);
+
+       // $token = JWTAuth::fromUser($user);
+        //dd($token); */
         return response()->json(Promocao::with('Produtos')->get());
     }
 
 
     public function getCategorias()
     {
-
-        return response()->json(Categoria::all()->pluck('nome_categoria','id'));
+        return response()->json(Categoria::all());
 
     }
 
